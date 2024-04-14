8e23199cf3f119a4457871c68da7b82283e9ad65
Evgeniy Zakhartsev <zaharcev777@ya.ru>
2024-04-14 23:19:00 +0300

    fixed data location

--- src/hello.py
+++ src/hello.py
@@ -5,8 +5,9 @@ import streamlit as st
 
 
 def get_zp_data():
-    df_zp_after_2017 = st.cache_data(pd.read_excel)("./data/tab3-zpl_2023.xlsx", sheet_name=0, skiprows=4)
-    df_zp_before_2017 = st.cache_data(pd.read_excel)("./data/tab3-zpl_2023.xlsx", sheet_name=1, skiprows=2)
+    url = "https://raw.githubusercontent.com/peaceful-djony/hse_ds_stepik/main/data/tab3-zpl_2023.xlsx"
+    df_zp_after_2017 = st.cache_data(pd.read_excel)(url, sheet_name=0, skiprows=4)
+    df_zp_before_2017 = st.cache_data(pd.read_excel)(url, sheet_name=1, skiprows=2)
 
     # Номер строки в excel с учетом пропуска первых неинформативных строк (параметр skiprows) и строки с заголовком
     df_zp = df_zp_before_2017.iloc[[28, 29]].reset_index(drop=True)
